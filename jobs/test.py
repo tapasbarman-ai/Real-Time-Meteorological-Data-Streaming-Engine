08fbb5858c750f391e9db4f56796c33b9fcf3951
Tapas Barman <tapasb.dev@gmail.com>
2026-06-04 12:34:16 +0530

    feat: initialize pyflink playground test suite

---
+++ jobs/test.py
@@ -0,0 +1,13 @@
+# pyflink working check 
+"""from pyflink.datastream import StreamExecutionEnvironment
+print("pyflink working correctly!")"""
+
+# pyflink enviroment
+
+"""from pyflink.datastream import StreamExecutionEnvironment
+
+env = StreamExecutionEnvironment.get_execution_environment()
+env.set_parallelism(1) # set parallelism to 1 for testing
+
+print("pyflink environment set up correctly!")
+"""
